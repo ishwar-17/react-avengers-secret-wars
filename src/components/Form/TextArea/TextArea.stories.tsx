c7699151f73bf4bbef34e28cd9fa84db6875fc0c
Ishwar <ishwardeoolkar171@gmail.com>
2024-03-11 15:28:53 +0530

    Added the textarea components...

--- src/components/Form/TextArea/TextArea.stories.tsx
+++ src/components/Form/TextArea/TextArea.stories.tsx
@@ -19,6 +19,89 @@ const StoryContainer = (props: any) => {
     );
 };
 
+const storiesArgTypes = {
+  argTypes: {
+    variant: {
+      name: "variant",
+      control: {
+        type: "radio",
+        labels: {
+          outlined: "outlined",
+          filled: "filled"
+        },
+      },
+      options: ["outlined", "filled"],
+    },
+    shape: {
+      name: "shape",
+      control: {
+        type: "radio",
+        labels: {
+          normal: "normal",
+          rounded: "rounded"
+        },
+      },
+      options: ["normal", "rounded"],
+    },
+    label: {
+      name: "label",
+      control: {
+        type: "radio",
+        labels: {
+          "": "Without Label",
+          label: "With Label"
+        },
+      },
+      options: ["", "label"],
+      mapping: {
+        label: "Message",
+      },
+    },
+  }
+}
+
 export const Default: Story = {
-    render: StoryContainer,
+  render: StoryContainer,
+  args: {
+    variant: "filled",
+    shape: "rounded",
+    label: "Subject",
+    name: 'subject',
+    value: 'Welcome to reactjs',
+    defaultValue: '',
+    error: false,
+    success: false,
+    placeholder: "Enter your message here..."
+  },
+  ...storiesArgTypes
+}
+
+export const Error: Story = {
+  render: StoryContainer,
+  args: {
+    variant: "outlined",
+    shape: "rounded",
+    label: "Subject", 
+    error: true,
+    success: false,
+    name: 'subject',
+    value: '',
+    defaultValue: '',
+    placeholder: "Error"
+  }
+}
+
+export const Success: Story = {
+  render: StoryContainer,
+  args: {
+    variant: "outlined",
+    shape: "rounded",
+    label: "Subject", 
+    error: false,
+    success: true,
+    name: 'subject',
+    value: 'Welcome to reactjs',
+    defaultValue: '',
+    placeholder: "Error"
+  }
 }
